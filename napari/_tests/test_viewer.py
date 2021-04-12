7aac46494d51a41c6f1e63e0e01e22e82e3e86ae
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2021-04-12 16:43:01 -0500

    Test

--- napari/_tests/test_viewer.py
+++ napari/_tests/test_viewer.py
@@ -164,21 +164,18 @@ def test_screenshot(make_napari_viewer):
     assert screenshot.ndim == 3
 
 
-def test_changing_theme(make_napari_viewer, qtbot):
+def test_changing_theme(make_napari_viewer):
     """Test changing the theme updates the full window."""
     viewer = make_napari_viewer(show=False, welcome=False)
     viewer.add_points(data=None)
-    qtbot.wait(2000)
     size = viewer.window.qt_viewer.size()
     viewer.window.qt_viewer.setFixedSize(size)
 
     assert viewer.theme == 'dark'
-    qtbot.wait(2000)
     screenshot_dark = viewer.screenshot(canvas_only=False)
 
     viewer.theme = 'light'
     assert viewer.theme == 'light'
-    qtbot.wait(2000)
     screenshot_light = viewer.screenshot(canvas_only=False)
 
     equal = (screenshot_dark == screenshot_light).min(-1)
